6ef85e8d229ee715120c9ad72c6a8cbce17ade62
wideweb <alexander@awideweb.com>
2025-06-15 00:20:25 -0400

    FIXED: Remove Next.js workflow and update dependencies

--- src/components/RecentPolls.tsx
+++ src/components/RecentPolls.tsx
@@ -11,7 +11,6 @@ import {
   CodeBracketIcon
 } from "@heroicons/react/24/outline";
 import { useRouter } from "next/router";
-import "tailwindcss/tailwind.css" fromm
 interface Choice {
   id: string;
   text: string;
