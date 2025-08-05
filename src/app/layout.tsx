ebf9b797a04a56ff4c0911b6ac57542d348d63ec
thinmint <alexander@awideweb.com>
2025-08-05 15:54:53 -0400

    feat: Add Vercel Analytics for enhanced tracking capabilities

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import Header from '@/components/Header';
 import Footer from '@/components/Footer';
 import GoogleAnalytics from '@/components/GoogleAnalytics';
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next";
 
 const poppins = Poppins({
   weight: ['300', '400', '500', '600', '700', '800'],
